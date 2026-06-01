69097351368792f1f98c14578be7a3e790bc0f22
ronmar <sigma4726spring@gmail.com>
2026-05-31 23:22:21 -0600

    feat(arrays-hashing): update top k frequent problem

--- arrays-hashing/top_k_frequent.py
+++ arrays-hashing/top_k_frequent.py
@@ -1,6 +1,7 @@
 
-# Time complexity: O(nlogn)
-# Space complexity: O(n)
+# Time complexity: O(n + m log m)
+# Space complexity: O(m)
+# where n is the number of elements in array nums and m is the number of unique elements in array nums.
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = defaultdict(int)
@@ -11,3 +12,68 @@ class Solution:
         return list(sorted(freq, key=freq.get, reverse=True)[:k])
         
        
+
+
+#Solution 1: 1. Sorting
+# Time complexity: O(nlogn)
+# Space complexity: O(n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
+
+
+# Solution 2: 2. Min-Heap
+# Time complexity: O(nlogk)
+# Space complexity: O(n+k)
+# Where n is the length of the array and k is the number of top frequent elements.
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+
+# Solution 3: 3. Bucket Sort
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
