855c18e0bd6403c353506281bb1abd749a3d195e
ronmar <sigma4726spring@gmail.com>
2026-09-01 23:52:34 -0600

    feat: add longest consecutive problem

---
+++ arrays-hashing/longest_consecutive.py
@@ -0,0 +1,77 @@
+# Solutio 1: 1. Brute Force
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0
+        store = set(nums)
+
+        for num in nums:
+            streak, curr = 0, num
+            while curr in store:
+                streak += 1
+                curr += 1
+            res = max(res, streak)
+        return res
+
+
+# Solutio 1: 2. Sorting
+# Time complexity: O(nlogn)
+# Space complexity: O(1) or O(n) depending on the sorting algorithm
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        res = 0
+        nums.sort()
+
+        curr, streak = nums[0], 0
+        i = 0
+        while i < len(nums):
+            if curr != nums[i]:
+                curr = nums[i]
+                streak = 0
+            while i < len(nums) and nums[i] == curr:
+                i += 1
+            streak += 1
+            curr += 1
+            res = max(res, streak)
+        return res
+
+
+# Solutio 1: 3. Hash Set
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+
+
+# Solutio 1: 4. Hash Map
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        res = 0
+
+        for num in nums:
+            if not mp[num]:
+                mp[num] = mp[num - 1] + mp[num + 1] + 1
+                mp[num - mp[num - 1]] = mp[num]
+                mp[num + mp[num + 1]] = mp[num]
+                res = max(res, mp[num])
+        return res
